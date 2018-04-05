c5b75d12addbd6c88ab12e4dc8cf678b6475267e
darshanime <deathbullet@gmail.com>
2018-04-06 01:03:41 +0530

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+hugo -t hugo-theme-cactus-plus
+cd public
+git add .
+
+msg="updating site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin master
+
+echo -e "\033[0;32mDone\033[0m"
+
+cd ..
